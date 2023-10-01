9bbd1ae0796ca7ab530cea81253ce1bcf397c13f
AlBelHub <125362352+AlBelHub@users.noreply.github.com>
2023-10-01 13:56:15 +0300

    Базовый функционал, добавил горизонтальный скролл

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
-import React, { SyntheticEvent, useState, useRef, useEffect } from 'react';
+import React, { SyntheticEvent, useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addBlock, deleteBlock } from "./store/basicActionsSlice.tsx";
 
-import Block from './Block.tsx';
+import Block from "./Block.tsx";
 import "./App.css";
 import { RootState } from "./store/store.tsx";
-import { nanoid } from 'nanoid';
+import { nanoid } from "nanoid";
 
 function App() {
-
   const [header, setHeader] = useState("");
 
-
-
-  const CardState = useSelector((state: RootState):any => state.basicAction.values);
+  const CardState = useSelector(
+    (state: RootState): any => state.basicAction.values
+  );
   const dispatch = useDispatch();
-
 
   const inputRef = useRef<HTMLInputElement>(null);
   const scrollContainer = useRef<HTMLDivElement>(null);
@@ -24,32 +22,43 @@ function App() {
     if (header.length === 0) {
       inputRef.current.focus();
     } else {
-      dispatch(addBlock({id: nanoid(4), header: header}))
-      
+      dispatch(addBlock({ id: nanoid(4), header: header }));
     }
-  } 
+  };
 
-  const handleWheel = (e:SyntheticEvent) => {
-      console.log("handled");
-      scrollContainer.current.scrollLeft += e.deltaY;
-  }
+  const handleWheel = (e: SyntheticEvent) => {
+    console.log("handled");
+    scrollContainer.current.scrollLeft += e.deltaY;
+  };
 
   return (
     <>
-        <div className="menu-container">
-          <p className="text">INFO</p>
-          <div className="count-TEMP">{CardState.length}</div>
-          <input ref={inputRef} type="text" onChange={e => { setHeader(e.target.value)}} />
-          <div className="button" onClick={() => handleAddButton()}>ADD</div>
-          <div className="button" onClick={() => dispatch(deleteBlock())}>DELETE</div>
+      <div className="menu-container">
+        <p className="text">INFO</p>
+        <div className="count-TEMP">{CardState.length}</div>
+        <input
+          ref={inputRef}
+          type="text"
+          onChange={(e) => {
+            setHeader(e.target.value);
+          }}
+        />
+        <div className="button" onClick={() => handleAddButton()}>
+          ADD
         </div>
-        <div className="cards-container" ref={scrollContainer} onWheel={(e) => handleWheel(e)}>
-          {CardState.map(card => {
-            return(
-              <Block key={card.id} header={card.header}/>
-            );
-          })}
+        <div className="button" onClick={() => dispatch(deleteBlock())}>
+          DELETE
         </div>
+      </div>
+      <div
+        className="cards-container"
+        ref={scrollContainer}
+        onWheel={(e) => handleWheel(e)}
+      >
+        {CardState.map((card) => {
+          return <Block key={card.id} header={card.header} />;
+        })}
+      </div>
     </>
   );
 }
