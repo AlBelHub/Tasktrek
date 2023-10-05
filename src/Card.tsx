29ad597a1160ae6799bcc5c553f8f82ca971cb3f
AlBelHub <125362352+AlBelHub@users.noreply.github.com>
2023-10-05 17:36:41 +0300

    Можно открыть оверлей (пока временный)

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { CardOverlay } from "./CardOverlay";
 
 interface Props {
   id: string;
@@ -7,15 +10,21 @@ interface Props {
 }
 
 export const Card = ({ id, header, BlockID }: Props) => {
+  const [openCard, setOpenCard] = useState(false);
+
   return (
     <>
-      <div className="card card_m-p card_border">
+      <div
+        className="card card_m-p card_border"
+        onClick={() => setOpenCard(!openCard)}
+      >
         <div className="card__tag-container">
           <div className="card__tag">Важно!</div>
         </div>
         <p className="card__header">{header}</p>
         <div className="card__date">02.08</div>
       </div>
+      {openCard && <CardOverlay setOpenCard={setOpenCard} />}
     </>
   );
 };
